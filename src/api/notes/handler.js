49b58c89076f6d1ac3510a27b11f2e4d3aff5a56
faysarystitha <etsukofr21d@student.unhas.ac.id>
2023-05-21 16:51:07 +0800

    export notes implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -21,7 +21,7 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload)
       const { title = 'untitled', body, tags } = request.payload
-      const { id: credentialId } = request.auth.credentials
+      const { id: credentialId } = request.auth.credentials // user id dari pemilik access token
 
       const noteId = await this._service.addNote({ title, body, tags, owner: credentialId })
 
--- src/server.js
+++ src/server.js
@@ -22,6 +22,10 @@ const collaborations = require('./api/collaborations')
 const CollaborationsService = require('./services/postgres/CollaborationsService')
 const CollaborationsValidator = require('./validator/collaborations')
 
+const _exports = require('./api/exports') // memakai _ karena kata exports merupakan salah satu member dari objek module
+const ProducerService = require('./services/rabbitmq/ProducerService')
+const ExportsValidator = require('./validator/exports')
+
 const init = async () => {
   const collaborationsService = new CollaborationsService()
   const notesService = new NotesService(collaborationsService)
@@ -92,6 +96,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator
+      }
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator
       }
     }
   ])
